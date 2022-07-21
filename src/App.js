28e998963b0f15a6fc85923e2a0d1cb96be1bc07
Rostislav <rostikshafar@gmail.com>
2022-07-21 21:11:16 +0300

    json 5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import Home from './Components/Home/Home.js';
 import { BrowserRouter,Route} from 'react-router-dom';
-import Config from './Firebase/Config';
 
 const App = () => {
-    Config.initializeFirebase();
 
     return(         
         <BrowserRouter>
