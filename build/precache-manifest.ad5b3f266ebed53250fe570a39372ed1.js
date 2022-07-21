0afca8f4ed1688b65743ab9c6216776ec9d5401b
Rostislav <rostikshafar@gmail.com>
2022-07-21 21:14:42 +0300

    json 6

---
+++ build/precache-manifest.ad5b3f266ebed53250fe570a39372ed1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "8643867cf3bb4bf32ec8",
+    "url": "/static/css/main.5aabf474.chunk.css"
+  },
+  {
+    "revision": "8643867cf3bb4bf32ec8",
+    "url": "/static/js/main.8643867c.chunk.js"
+  },
+  {
+    "revision": "545a37d60292da53c8c8",
+    "url": "/static/js/1.545a37d6.chunk.js"
+  },
+  {
+    "revision": "2ce9f3e9c77d3437c269",
+    "url": "/static/js/2.2ce9f3e9.chunk.js"
+  },
+  {
+    "revision": "2214ee01492315b23d2d",
+    "url": "/static/js/runtime~main.2214ee01.js"
+  },
+  {
+    "revision": "9bac4223f4cd711b4149afe58f8a4de1",
+    "url": "/static/media/video.9bac4223.mp4"
+  },
+  {
+    "revision": "e85ce6a6d35d7437c0aec9349ed9ce90",
+    "url": "/static/media/IMG_1.e85ce6a6.jpeg"
+  },
+  {
+    "revision": "e705ee91f56bc43c05cbcdbc17b1fbf9",
+    "url": "/static/media/IMG_2.e705ee91.jpg"
+  },
+  {
+    "revision": "a637880d5db86e0fe51c34a86ecee19d",
+    "url": "/static/media/IMG_3.a637880d.jpg"
+  },
+  {
+    "revision": "9db5a6aca30c2c78d8e95523790491e4",
+    "url": "/static/media/photo.9db5a6ac.jpeg"
+  },
+  {
+    "revision": "85bb2d0ec4a0da159de42e89089ccc0b",
+    "url": "/static/media/Pacifico.85bb2d0e.ttf"
+  },
+  {
+    "revision": "8e0807fa4a2e57d48d802bdc0e140e25",
+    "url": "/index.html"
+  }
+];
