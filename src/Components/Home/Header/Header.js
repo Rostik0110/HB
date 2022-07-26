27fdf929e68cac91e9b50b4c06d36330d5ad14ed
Rostislav <rostikshafar@gmail.com>
2022-07-26 21:01:03 +0300

    autoplay 1

--- src/Components/Home/Header/Header.js
+++ src/Components/Home/Header/Header.js
@@ -4,7 +4,9 @@ import video from "./video.mp4"
 const Header = () =>{
     return(
         <div className="HeaderConteiner">
-            <video controls={false} autoplay loop muted playsinline webkit-playinginline className="Video" src={video}></video>
+            <video controls={false} autoplay loop muted playsinline webkit-playinginline className="Video" >
+                <source src={video} type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
+            </video>
         </div>
     )
 }
